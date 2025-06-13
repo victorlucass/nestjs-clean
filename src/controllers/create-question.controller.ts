a108d90f49756673b0daf66a79f847ea1e680a48
Victor Lucas <victorlucas.ao@gmail.com>
2025-06-12 22:46:51 -0400

    Controller de criação de pergunta

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,17 +1,37 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { UserPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
 
-// const createQuestionBodySchema = z.object({})
-// type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
+  constructor(private prisma: PrismaService) {}
+
   @Post()
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log(user.sub)
-    return 'ok'
+  async handle(
+    @Body(new ZodValidationPipe(createQuestionBodySchema))
+    body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, content } = body
+
+    await this.prisma.question.create({
+      data: {
+        title,
+        content,
+        slug: title.toLowerCase().replace(/ /g, '-'),
+        authorId: user.sub,
+      },
+    })
   }
 }
